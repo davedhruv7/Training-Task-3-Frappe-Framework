deb7f478b209a5d64c3f981b5bf6aaf8973ec8fb
Dhruv Dave <dxdave07012@gmail.com>
2025-02-06 12:09:44 +0530

    Server Side Calls Added

--- library_management/user_management/doctype/client_side_scripting/client_side_scripting.py
+++ library_management/user_management/doctype/client_side_scripting/client_side_scripting.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Dhruv and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ClientSideScripting(Document):
 	pass
+
+@frappe.whitelist()
+def frappe_call(msg):
+	# import time
+	# time.sleep(5)
+	# frappe.msgprint(msg)
+
+	return "Hi This Message from frappe call"
--- library_management/user_management/doctype/server/server.js
+++ library_management/user_management/doctype/server/server.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2025, Dhruv and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Server", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Server", {
+	enable(frm) {
+        frappe.call({
+            method: "apps.library_management.library_management.user_management.doctype.client_side_scripting.client_side_scripting.frappe_call",
+            args:{
+                msg: "Hello"
+            },
+            frappe: true,
+            freeze_message: __('Calling frappe_call Method'),
+            callback: function(r) {
+                frappe.msgprint(r.method)
+            }
+        })
+	},
+});
